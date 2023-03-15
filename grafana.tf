1b12b95d339db98aedd3f416ab3ac536236ce344
Matt Bialas <mbialas@element84.com>
2023-03-15 14:08:31 -0400

    Addressing PR feedback

--- grafana.tf
+++ grafana.tf
@@ -1,21 +1,3 @@
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    annotations = {
-      "linkerd.io/inject" = "enabled"
-    }
-
-    labels = {
-      app = "monitoring"
-    }
-
-    name = "monitoring"
-  }
-
-  depends_on = [
-    helm_release.ingress_nginx
-  ]
-}
-
 resource "helm_release" "grafana" {
   chart            = "grafana"
   name             = "grafana"
