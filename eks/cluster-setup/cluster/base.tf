033343a2e1872238f70051fae5479e54dd4b6245
Paul Pilone <paul@element84.com>
2023-03-24 10:18:04 -0400

    Fixes typo in eks addon name

--- eks/cluster-setup/cluster/base.tf
+++ eks/cluster-setup/cluster/base.tf
@@ -77,7 +77,7 @@ resource "aws_eks_node_group" "node_group" {
   ]
 }
 
-resource "aws_eks_addon" "ebs-csi-dirver-addon" {
+resource "aws_eks_addon" "ebs-csi-driver-addon" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "aws-ebs-csi-driver"
   addon_version      = "v1.17.0-eksbuild.1"
