6be8f432e0c0a38e18ef6c396f4711d5c60f7a46
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-07-24 09:07:37 -0500

    KubernetesJob to initialize database

--- modules/db/postgres/main.tf
+++ modules/db/postgres/main.tf
@@ -1,17 +1,17 @@
 resource "helm_release" "postgres" {
-  name = "postgres"
-  namespace = var.namespace
-  repository = "https://element84.github.io/filmdrop-k8s-helm-charts"
-  chart = "postgres"
-  version = var.postgres_version
-  atomic = true
+  name             = "postgres"
+  namespace        = var.namespace
+  repository       = "https://element84.github.io/filmdrop-k8s-helm-charts"
+  chart            = "postgres"
+  version          = var.postgres_version
+  atomic           = true
   create_namespace = false
 
   dynamic "set" {
     for_each = var.custom_input_map
 
     content {
-      name = set.key
+      name  = set.key
       value = set.value
     }
   }
@@ -20,4 +20,82 @@ resource "helm_release" "postgres" {
     [var.custom_postgres_values_yaml == "" ? file("${path.module}/values.yaml") : file(var.custom_postgres_values_yaml)],
     length(var.postgres_additional_configuration_values) == 0 ? [] : var.postgres_additional_configuration_values
   )
+}
+
+module "dbinit" {
+
+  source = "../../jobs"
+  depends_on = [kubernetes_secret.db_postgres_secret_owner_role,
+    kubernetes_secret.db_postgres_secret_api_role,
+    kubernetes_secret.db_postgres_secret_caboose_role,
+  kubernetes_secret.db_postgres_secret_conductor_role]
+
+}
+
+resource "kubernetes_secret" "db_postgres_secret_owner_role" {
+  metadata {
+    name      = "postgres-secret-owner-role"
+    namespace = var.namespace
+  }
+
+  binary_data = {
+    username = "dXNlcl9vd25lcg=="
+    password = "cGFzc19vd25lcg=="
+  }
+
+  depends_on = [
+    helm_release.postgres
+  ]
+
+}
+
+resource "kubernetes_secret" "db_postgres_secret_api_role" {
+  metadata {
+    name      = "postgres-secret-api-role"
+    namespace = var.namespace
+  }
+
+  binary_data = {
+    username = "dXNlcl9hcGk="
+    password = "cGFzc19hcGk="
+  }
+
+  depends_on = [
+    helm_release.postgres
+  ]
+
+}
+
+resource "kubernetes_secret" "db_postgres_secret_caboose_role" {
+  metadata {
+    name      = "postgres-secret-caboose-role"
+    namespace = var.namespace
+  }
+
+  binary_data = {
+    username = "dXNlcl9jYWJvb3Nl"
+    password = "cGFzc19jYWJvb3Nl"
+  }
+
+  depends_on = [
+    helm_release.postgres
+  ]
+
+}
+
+resource "kubernetes_secret" "db_postgres_secret_conductor_role" {
+  metadata {
+    name      = "postgres-secret-conductor-role"
+    namespace = var.namespace
+  }
+
+  binary_data = {
+    username = "dXNlcl9jb25kdWN0b3I="
+    password = "cGFzc19jb25kdWN0b3I="
+  }
+
+  depends_on = [
+    helm_release.postgres
+  ]
+
 }
---
+++ modules/jobs/dbInit.tf
@@ -0,0 +1,98 @@
+resource "kubernetes_job_v1" "db-initialization" {
+  metadata {
+    name      = "db-initialization"
+    namespace = "db"
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        container {
+          name    = "swoop-db"
+          image   = "quay.io/element84/swoop-db"
+          command = ["python", "home/db-initialization.py"]
+          env {
+            name = "OWNER_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-owner-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "OWNER_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-owner-role"
+                key  = "password"
+              }
+            }
+          }
+          env {
+            name = "API_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-api-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "API_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-api-role"
+                key  = "password"
+              }
+            }
+          }
+          env {
+            name = "CABOOSE_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-caboose-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "CABOOSE_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-caboose-role"
+                key  = "password"
+              }
+            }
+          }
+          env {
+            name = "CONDUCTOR_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-conductor-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "CONDUCTOR_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-conductor-role"
+                key  = "password"
+              }
+            }
+          }
+
+        }
+        restart_policy = "Never"
+      }
+    }
+    backoff_limit = 4
+  }
+  wait_for_completion = true
+  timeouts {
+    create = "2m"
+    update = "2m"
+  }
+}
