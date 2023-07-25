e735647a5b3484a6997970873e265da62e332e8b
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-07-25 14:36:16 -0500

    KubernetesJob to initialize database (#33)

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -8,14 +8,14 @@ module "db_namespace" {
 
 
 module "postgres" {
-  source  = "./postgres"
-  count   = var.deploy_postgres == true ? 1 : 0
+  source = "./postgres"
+  count  = var.deploy_postgres == true ? 1 : 0
 
-  namespace                                 = var.namespace
-  postgres_version                          = var.postgres_version
-  postgres_additional_configuration_values  = var.postgres_additional_configuration_values
-  custom_postgres_values_yaml               = var.custom_postgres_values_yaml
-  custom_input_map                          = var.custom_input_map
+  namespace                                = var.namespace
+  postgres_version                         = var.postgres_version
+  postgres_additional_configuration_values = var.postgres_additional_configuration_values
+  custom_postgres_values_yaml              = var.custom_postgres_values_yaml
+  custom_input_map                         = var.custom_input_map
 
   depends_on = [module.db_namespace]
 }
---
+++ modules/jobs/dbInit.tf
@@ -0,0 +1,120 @@
+resource "kubernetes_job_v1" "db-initialization" {
+  metadata {
+    name      = "db-initialization"
+    namespace = "db"
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        container {
+          name    = "swoop-db"
+          image   = "quay.io/element84/swoop-db"
+          command = ["python", "opt/swoop/db/scripts/db-initialization.py"]
+
+          env {
+            name  = "PGHOST"
+            value = "postgres"
+          }
+
+          env {
+            name  = "PGUSER"
+            value = "postgres"
+          }
+
+          env {
+            name  = "PGDATABASE"
+            value = "swoop"
+          }
+
+          env {
+            name  = "PGPORT"
+            value = "5432"
+          }
+
+          env {
+            name = "API_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-api-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "API_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-api-role"
+                key  = "password"
+              }
+            }
+          }
+          env {
+            name = "CABOOSE_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-caboose-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "CABOOSE_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-caboose-role"
+                key  = "password"
+              }
+            }
+          }
+          env {
+            name = "CONDUCTOR_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-conductor-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "CONDUCTOR_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-conductor-role"
+                key  = "password"
+              }
+            }
+          }
+
+          env {
+            name = "MIGRATION_ROLE_USER"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-migration-role"
+                key  = "username"
+              }
+            }
+          }
+          env {
+            name = "MIGRATION_ROLE_PASS"
+            value_from {
+              secret_key_ref {
+                name = "postgres-secret-migration-role"
+                key  = "password"
+              }
+            }
+          }
+
+        }
+        restart_policy = "Never"
+      }
+    }
+    backoff_limit = 4
+  }
+  wait_for_completion = true
+  timeouts {
+    create = "2m"
+    update = "2m"
+  }
+}
