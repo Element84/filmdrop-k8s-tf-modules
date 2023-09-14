81e88c83604b4bdda8b2bcbf68d55094b0bcf01f
Hector Machin <hector@element84.com>
2023-09-14 17:21:44 -0400

    Adding changes to support latest swoop-config.yaml and conductor callback functionality

--- modules/swoop/inputs.tf
+++ modules/swoop/inputs.tf
@@ -102,7 +102,7 @@ variable custom_swoop_input_map {
     "swoop.conductor.argo.enabled"      = false
     "swoop.conductor.argo.crdsInstall"  = false
     "swoop.conductor.argo.objectCounts" = 5
-    "swoop.conductor.instanceName"      = "instance-a"
+    "swoop.conductor.instanceName"      = "main"
     "swoop.bundle.serviceAccount"       = "swoop-bundle"
   }
 }
@@ -208,4 +208,52 @@ variable swoop_sa_iam_role {
   description   = "IAM Role for SWOOP Service Account"
   default       = ""
   sensitive     = true
+}
+
+variable custom_stac_fastapi_input_map {
+  type        = map
+  description = "Input values for STAC-FastAPI Helm Chart"
+  default = {
+    "local-path-provisioner.enabled"                      = true
+    "local-path-provisioner.singleNamespace"              = false
+    "local-path-provisioner.storageClass.provisionerName" = "filmdrop.io/local-pgstac-path-provisioner"
+    "local-path-provisioner.storageClass.name"            = "local-path-class-pgstac"
+    "local-path-provisioner.configmap.name"               = "local-path-config-pgstac"
+    "pgStac.container.port"                               = 5432
+    "pgStac.deployment.name"                              = "pgstac"
+    "pgStac.image.repository"                             = "quay.io/element84/pgstac"
+    "pgStac.image.tag"                                    = "latest"
+    "pgStac.enabled"                                      = true
+    "pgStac.replicaCount"                                 = 1
+    "pgStac.service.type"                                 = "ClusterIP"
+    "pgStac.service.port"                                 = 5439
+    "pgStac.service.targetPort"                           = 5432
+    "pgStac.service.name"                                 = "pgstac"
+    "pgStac.dbName"                                       = "postgis"
+    "pgStac.dbHost"                                       = ""
+    "pgStac.dbUser"                                       = ""
+    "pgStac.dbPassword"                                   = ""
+    "pgStac.createPgStacSecret"                           = false
+    "pgStac.storage.size"                                 = "256M"
+    "pgStac.storage.volumeBindingMode"                    = "WaitForFirstConsumer"
+    "pgStac.storage.provisioner"                          = "filmdrop.io/local-pgstac-path-provisioner"
+    "pgStac.storage.retainPersistentVolume"               = true
+    "pgStac.storage.storageClassName"                     = "pgstac-retain"
+    "stacFastApi.container.port"                          = 8080
+    "stacFastApi.deployment.name"                         = "stac-fastapi-pgstac"
+    "stacFastApi.image.repository"                        = "ghcr.io/stac-utils/stac-fastapi-pgstac"
+    "stacFastApi.image.tag"                               = "main"
+    "stacFastApi.replicaCount"                            = 1
+    "stacFastApi.service.type"                            = "ClusterIP"
+    "stacFastApi.service.port"                            = 8080
+    "stacFastApi.service.targetPort"                      = 8080
+    "stacFastApi.service.name"                            = "stac-fastapi-pgstac"
+    "stacFastApi.service.serviceserviceAccount"           = "stac-fastapi-pgstac"
+  }
+}
+
+variable stac_namespace {
+  type        = string
+  description = "Namespace for STAC-FastAPI"
+  default     = "stac"
 }
